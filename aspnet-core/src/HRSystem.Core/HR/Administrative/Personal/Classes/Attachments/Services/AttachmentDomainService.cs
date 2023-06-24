b046261e2cf441a1ae1eb1679bdaa20c08c1c1eb
AmerSouccar <AmerSouccar@hotmail.com>
2023-06-24 18:23:09 +0300

    Attachments

---
+++ aspnet-core/src/HRSystem.Core/HR/Administrative/Personal/Classes/Attachments/Services/AttachmentDomainService.cs
@@ -0,0 +1,59 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Runtime.Session;
+using HRSystem.Authorization.Users;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Administrative.Personal.Classes.Attachments.Services
+{
+    public class AttachmentDomainService : IAttachmentDomainService
+    {
+        private readonly IRepository<Attachment, Guid> _attachmentRepository;
+        private readonly IAbpSession abpSession;
+        private readonly UserManager userManager;
+        private IWebHostEnvironment _environment;
+
+        public AttachmentDomainService(IRepository<Attachment, Guid> attachmentRepository, IAbpSession abpSession, UserManager userManager, IWebHostEnvironment environment)
+        {
+            _attachmentRepository = attachmentRepository;
+            this.abpSession = abpSession;
+            this.userManager = userManager;
+            _environment = environment;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await _attachmentRepository.DeleteAsync(id);
+        }
+
+        public async Task<Attachment> Insert(IFormFile file, string title, string description)
+        {
+            if (!Directory.Exists(_environment.WebRootPath + "\\Attachments\\"))
+            {
+                Directory.CreateDirectory(_environment.WebRootPath + "\\Attachments\\");
+            }
+            var DT = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+
+            using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Attachments\\" + DT + "_" + file.FileName))
+            {
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            Attachment attachment = new Attachment()
+            {
+                Title = title,
+                Description = description,
+                FilePath = DT + "_" + file.FileName,
+            };
+            await _attachmentRepository.InsertAsync(attachment);
+            return (attachment);
+        }
+    }
+}
---
+++ aspnet-core/src/HRSystem.Core/HR/Administrative/Personal/Classes/Banks/Services/BankDomainService.cs
@@ -0,0 +1,44 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Administrative.Personal.Classes.Banks.Services
+{
+    public class BankDomainService : IBankDomainService
+    {
+        private readonly IRepository<Bank, Guid> _bankRepository;
+
+        public BankDomainService(IRepository<Bank, Guid> bankRepository)
+        {
+            _bankRepository = bankRepository;
+        }
+
+        public async Task Delete(Guid id)
+        {
+         await _bankRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<Bank>> GetAll()
+        {
+            return _bankRepository.GetAllIncluding(x => x.Nationality).ToList();
+        }
+
+        public async Task<Bank> GetbyId(Guid id)
+        {
+            return _bankRepository.GetAllIncluding(x => x.Nationality).FirstOrDefault(x => x.Id == id);
+        }
+
+        public async Task<Bank> Insert(Bank bank)
+        {
+          return await _bankRepository.InsertAsync(bank);
+        }
+
+        public async Task<Bank> Update(Bank bank)
+        {
+            return await _bankRepository.UpdateAsync(bank);
+        }
+    }
+}
