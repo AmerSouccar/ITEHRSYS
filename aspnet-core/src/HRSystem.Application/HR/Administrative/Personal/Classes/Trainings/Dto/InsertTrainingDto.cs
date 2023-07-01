e786743ef903c7efe7339b0e118eda946674e65f
wessam1234 <131531361+wessam1234@users.noreply.github.com>
2023-07-01 22:28:04 +0300

    Training

---
+++ aspnet-core/src/HRSystem.Application/HR/Administrative/Personal/Classes/Trainings/Dto/InsertTrainingDto.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Entities.Auditing;
+using HRSystem.HR.Administrative.Personal.Classes.Attachments.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Administrative.Personal.Classes.Trainings.Dto
+{
+    public class InsertTrainingDto :EntityDto<Guid>
+    {
+        public class Training : FullAuditedAggregateRoot<Guid>
+        {
+            #region Course
+            public string Specialize { get; set; }
+            public string CourseName { get; set; }
+            public DateTime? CourseStartDate { get; set; }
+            public DateTime? CourseEndDate { get; set; }
+            public int? CourseDurationPerHour { get; set; }
+            #endregion
+            #region Training Center
+            public string TrainingCenter { get; set; }
+            public string TrainingCenterLocation { get; set; }
+            public string Instructor { get; set; }
+            public string InstructorPhone { get; set; }
+            public DateTime? CertificateIssuanceDate { get; set; }
+            public bool isActive { get; set; }
+            public string Notes { get; set; }
+
+            #endregion
+        }
+    }
+}
---
+++ aspnet-core/src/HRSystem.Application/HR/Administrative/Personal/Classes/Trainings/Services/TrainingAppService.cs
@@ -0,0 +1,44 @@
+﻿using HRSystem.HR.Administrative.Personal.Classes.Trainings.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Administrative.Personal.Classes.Trainings.Services
+{
+    public class TrainingAppService : HRSystemAppServiceBase, ITrainingAppService
+    {
+        private readonly ITrainingDomainService _traningDomainservice;
+
+        public TrainingAppService(ITrainingDomainService traningDomainservice)
+        {
+            _traningDomainservice = traningDomainservice;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await _traningDomainservice.Delete(id);
+        }
+
+        public async Task<List<ReadTrainingDto>> GetAll()
+        {
+           return ObjectMapper.Map<List<ReadTrainingDto>> (await _traningDomainservice.GetAll());
+        }
+
+        public async Task<ReadTrainingDto> GetbyId(Guid id)
+        {
+            return ObjectMapper.Map<ReadTrainingDto>(await _traningDomainservice.GetbyId(id));
+        }
+
+        public async Task<InsertTrainingDto> Insert(InsertTrainingDto training)
+        {
+            return ObjectMapper.Map<InsertTrainingDto>(await _traningDomainservice.Insert(ObjectMapper.Map<Training>(training)));
+        }
+
+        public async Task<UpdateTrainingDto> Update(UpdateTrainingDto training)
+        {
+            return ObjectMapper.Map<UpdateTrainingDto>(await _traningDomainservice.Update(ObjectMapper.Map<Training>(training)));
+        }
+    }
+}
