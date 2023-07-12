b3b35e7516d5a3dfcf36a3116c12ee2dcd1d96e0
AmerSouccar <AmerSouccar@hotmail.com>
2023-07-12 22:08:44 +0300

    OperationalDomain

---
+++ aspnet-core/src/HRSystem.Core/HR/Operational/AttendanceSystem/Classes/AttendanceForms/Services/AttendanceFormDomainService.cs
@@ -0,0 +1,49 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Operational.AttendanceSystem.Classes.AttendanceForms.Services
+{
+    public class AttendanceFormDomainService : IAttendanceFormDomainService
+    {
+        private readonly IRepository<AttendanceForm, Guid> _attendanceFormRepository;
+
+        public AttendanceFormDomainService(IRepository<AttendanceForm, Guid> attendanceFormRepository)
+        {
+            _attendanceFormRepository = attendanceFormRepository;
+        }
+
+        public async Task Delete(Guid id)
+        {
+           await _attendanceFormRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<AttendanceForm>> GetAll()
+        {
+            return _attendanceFormRepository.GetAllIncluding(x => x.Workshops).ToList();
+        }
+
+        public async Task<AttendanceForm> GetbyId(Guid id)
+        {
+            AttendanceForm attendanceForm = await _attendanceFormRepository.GetAsync(id);
+            if(attendanceForm != null)
+            {
+                await _attendanceFormRepository.EnsureCollectionLoadedAsync(attendanceForm, x => x.Workshops);
+            }
+            return attendanceForm;
+        }
+
+        public async Task<AttendanceForm> Insert(AttendanceForm attendanceForm)
+        {
+            return await _attendanceFormRepository.InsertAsync(attendanceForm);
+        }
+
+        public async Task<AttendanceForm> Update(AttendanceForm attendanceForm)
+        {
+            return await _attendanceFormRepository.UpdateAsync(attendanceForm);
+        }
+    }
+}
---
+++ aspnet-core/src/HRSystem.Core/HR/Operational/AttendanceSystem/Classes/AttendanceMonthlyCards/Services/AttendanceMonthlyCardDomainService.cs
@@ -0,0 +1,49 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.HR.Operational.AttendanceSystem.Classes.AttendanceMonthlyCards.Services
+{
+    public class AttendanceMonthlyCardDomainService : IAttendanceMonthlyCardDomainService
+    {
+        private readonly IRepository<AttendanceMonthlyCard, Guid> _attendanceMonthlyCardRepository;
+
+        public AttendanceMonthlyCardDomainService(IRepository<AttendanceMonthlyCard, Guid> attendanceMonthlyCardRepository)
+        {
+            _attendanceMonthlyCardRepository = attendanceMonthlyCardRepository;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await _attendanceMonthlyCardRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<AttendanceMonthlyCard>> GetAll()
+        {
+            return _attendanceMonthlyCardRepository.GetAllIncluding(x => x.EmployeeCard).ToList();
+        }
+
+        public async Task<AttendanceMonthlyCard> GetbyId(Guid id)
+        {
+            AttendanceMonthlyCard attendanceMonthlyCard = await _attendanceMonthlyCardRepository.GetAsync(id);
+            if(attendanceMonthlyCard != null)
+            {
+                await _attendanceMonthlyCardRepository.EnsurePropertyLoadedAsync(attendanceMonthlyCard, x => x.EmployeeCard);
+            }
+            return attendanceMonthlyCard;
+        }
+
+        public async Task<AttendanceMonthlyCard> Insert(AttendanceMonthlyCard attendanceMonthlyCard)
+        {
+            return await _attendanceMonthlyCardRepository.InsertAsync(attendanceMonthlyCard);
+        }
+
+        public async Task<AttendanceMonthlyCard> Update(AttendanceMonthlyCard attendanceMonthlyCard)
+        {
+            return await _attendanceMonthlyCardRepository.UpdateAsync(attendanceMonthlyCard);
+        }
+    }
+}
