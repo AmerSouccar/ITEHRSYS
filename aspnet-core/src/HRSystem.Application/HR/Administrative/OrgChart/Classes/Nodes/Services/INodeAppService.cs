75b5c026b0d9abd75756fc48bf484798d3e98801
wessam1234 <131531361+wessam1234@users.noreply.github.com>
2023-07-15 21:07:05 +0300

    NodeApi

--- aspnet-core/src/HRSystem.Application/HR/Administrative/OrgChart/Classes/Nodes/Services/INodeAppService.cs
+++ aspnet-core/src/HRSystem.Application/HR/Administrative/OrgChart/Classes/Nodes/Services/INodeAppService.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.Application.Services;
+using HRSystem.HR.Administrative.OrgChart.Classes.Nodes.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,10 @@ namespace HRSystem.HR.Administrative.OrgChart.Classes.Nodes.Services
 {
     public interface INodeAppService : IApplicationService
     {
+        public Task<List<ReadNodeDto>> GetAll();
+        public Task<ReadNodeDto> GetbyId(Guid id);
+        public Task<InsertNodeDto> Insert(InsertNodeDto insertNodeDto);
+        public Task<UpdateNodeDto> Update(UpdateNodeDto updateNodeDto);
+        public Task Delete(Guid id);
     }
 }
--- aspnet-core/src/HRSystem.Application/HR/Administrative/OrgChart/Classes/Nodes/Services/NodeAppService.cs
+++ aspnet-core/src/HRSystem.Application/HR/Administrative/OrgChart/Classes/Nodes/Services/NodeAppService.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Abp.Domain.Services;
+using AutoMapper.Internal.Mappers;
+using HRSystem.HR.Administrative.Grades.Classes.JobTitles;
+using HRSystem.HR.Administrative.Grades.Classes.JobTitles.Dto;
+using HRSystem.HR.Administrative.OrgChart.Classes.Nodes.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HRSystem.HR.Administrative.OrgChart.Classes.Nodes.Services
 {
-    public class NodeAppService : HRSystemAppServiceBase,INodeAppService
+    public class NodeAppService : HRSystemAppServiceBase, INodeAppService
     {
+        private readonly INodeDomainService _nodeDomainService;
+
+        public NodeAppService(INodeDomainService nodeDomainService)
+        {
+            _nodeDomainService = nodeDomainService;
+        }
+
+        public async Task Delete(Guid id)
+        {
+           await _nodeDomainService.Delete(id);
+        }
+
+        public async Task<List<ReadNodeDto>> GetAll()
+        {
+           return ObjectMapper.Map<List<ReadNodeDto>> (await _nodeDomainService.GetAll());
+        }
+
+        public async Task<ReadNodeDto> GetbyId(Guid id)
+        {
+            return ObjectMapper.Map<ReadNodeDto>(await _nodeDomainService.GetbyId(id));
+
+        }
+
+        public async Task<InsertNodeDto> Insert(InsertNodeDto insertNodeDto)
+        {
+            return ObjectMapper.Map<InsertNodeDto>(await _nodeDomainService.Insert(ObjectMapper.Map<Node>(insertNodeDto)));
+
+        }
+
+        public async Task<UpdateNodeDto> Update(UpdateNodeDto updateNodeDto)
+        {
+            return ObjectMapper.Map<UpdateNodeDto>(await _nodeDomainService.Update(ObjectMapper.Map<Node>(updateNodeDto)));
+
+        }
     }
 }
